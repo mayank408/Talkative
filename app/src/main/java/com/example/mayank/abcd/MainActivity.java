9bb0063f9126688b9b16a012161f1292e3ae29e2
mayank408 <mayank.trp48@gmail.com>
2016-12-02 13:26:02 +0530

    Login Created

--- app/src/main/java/com/example/mayank/abcd/MainActivity.java
+++ app/src/main/java/com/example/mayank/abcd/MainActivity.java
@@ -1,11 +1,27 @@
 package com.example.mayank.abcd;
 
+import android.app.ProgressDialog;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,25 +29,81 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.email)
-    TextView username;
-    @BindView(R.id.password_input)
-    TextView password;
+    private static final String URL = "http://sakshi.pythonanywhere.com/done";
 
-    Button login = (Button)findViewById(R.id.log_in);
+        TextView username , password;
+        ProgressDialog pd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+        username = (TextView) findViewById(R.id.email);
+        password = (TextView) findViewById(R.id.password_input);
+        Button login = (Button) findViewById(R.id.log_in);
+
+        pd=new ProgressDialog(MainActivity.this);
+        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        pd.setMessage("Loggin In....");
+
+
+
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
     }
 
-    String Username = username.getText().toString().trim();
-    String Password = password.getText().toString().trim();
+            private void registerUser() {
+                pd.show();
+                final String Username = username.getText().toString().trim();
+                final String Password = password.getText().toString().trim();
 
 
 
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                pd.hide();
+
+                                try {
+                                    JSONObject json = new JSONObject(response);
+                                    String str = json.getString("status");
+                                    Toast.makeText(MainActivity.this, str, Toast.LENGTH_LONG).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("username", Username);
+                        params.put("password", Password);
+                        return params;
+                    }
+
+                };
+
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
+            }
 
 
-}
+        }
+
---
+++ app/src/main/java/com/example/mayank/abcd/SignUp.java
@@ -0,0 +1,8 @@
+package com.example.mayank.abcd;
+
+/**
+ * Created by mayank on 02-12-2016.
+ */
+
+public class SignUp {
+}
